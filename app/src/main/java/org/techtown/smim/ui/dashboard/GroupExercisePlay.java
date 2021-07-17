49a80b5e74afeb249ba87822df9fcd1218a65e98
sia20181011 <sia20181011@duksung.ac.kr>
2021-07-17 18:40:21 +0900

    그룹상세페이지 그룹가입버튼추가, 그룹운동하기 페이지 만들기

---
+++ app/src/main/java/org/techtown/smim/ui/dashboard/GroupExercisePlay.java
@@ -0,0 +1,22 @@
+package org.techtown.smim.ui.dashboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import org.techtown.smim.R;
+
+import kr.co.prnd.YouTubePlayerView;
+
+public class GroupExercisePlay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_exercise_play);
+        YouTubePlayerView youtubePV = findViewById(R.id.youtube_player_view);
+
+        youtubePV.play("NDsjmxTROEo",null);
+    }
+}
