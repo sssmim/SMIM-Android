28a07ab5e5394523e0cb9f5b673ad8f80bea858d
Hyunseung <ardimento22@naver.com>
2021-08-01 00:52:37 +0900

    test페이지 추가

---
+++ app/src/main/java/org/techtown/smim/ui/home/personal.java
@@ -0,0 +1,11 @@
+package org.techtown.smim.ui.home;
+
+public class personal {
+    public int mem_num;
+    public String id;
+    public String pwd;
+    public String personal_image;
+    public String interest;
+    public String daily_record;
+    public String daily_total;
+}
--- app/src/main/java/org/techtown/smim/ui/home/test.java
+++ app/src/main/java/org/techtown/smim/ui/home/test.java
@@ -3,8 +3,27 @@ package org.techtown.smim.ui.home;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.android.volley.Cache;
+import com.android.volley.Network;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.StringRequest;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 
 import org.techtown.smim.R;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
 
 public class test extends AppCompatActivity {
 
@@ -13,6 +32,42 @@ public class test extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
 
+        TextView textView = findViewById(R.id.result);
+
+        RequestQueue requestQueue;
+
+        // Instantiate the cache
+        Cache cache = new DiskBasedCache(getCacheDir(), 1024 * 1024); // 1MB cap
+
+        // Set up the network to use HttpURLConnection as the HTTP client.
+        Network network = new BasicNetwork(new HurlStack());
+
+        // Instantiate the RequestQueue with the cache and network.
+        requestQueue = new RequestQueue(cache, network);
+
+        // Start the queue
+        requestQueue.start();
+
+        String url = "http://52.78.235.23:8080/personal";
+
+        // Formulate the request and handle the response.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Gson gson = new GsonBuilder().setPrettyPrinting().create();
+                Type listType = new TypeToken<ArrayList<personal>>(){}.getType();
+                List<personal> list = gson.fromJson(response, listType);
+                textView.setText(list.get(0).id);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                textView.setText("에러");
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        requestQueue.add(stringRequest);
 
     }
 }
