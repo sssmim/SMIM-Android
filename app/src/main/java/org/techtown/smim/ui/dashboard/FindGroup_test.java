1e00fc8679db4c8948ca6d2ec28415087cfdbc67
sia7997 <sia7997@naver.com>
2021-08-02 19:42:09 +0900

    그룹 리스트 post완료

--- app/src/main/java/org/techtown/smim/ui/dashboard/FindGroup_test.java
+++ app/src/main/java/org/techtown/smim/ui/dashboard/FindGroup_test.java
@@ -2,6 +2,7 @@ package org.techtown.smim.ui.dashboard;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,14 +15,18 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Cache;
 import com.android.volley.Network;
+import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.BasicNetwork;
 import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HttpHeaderParser;
 import com.android.volley.toolbox.HurlStack;
 import com.android.volley.toolbox.StringRequest;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -29,13 +34,17 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.techtown.smim.R;
 import org.techtown.smim.database.group;
 
 import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class FindGroup_test extends Fragment {
     public static final int num1 = 326;
@@ -50,7 +59,7 @@ public class FindGroup_test extends Fragment {
         dashboardViewModel =
                 new ViewModelProvider(this).get(DashboardViewModel.class);
         View root = (View)inflater.inflate(R.layout.group_find, container, false);
-
+        Button post = (Button)root.findViewById(R.id.post);
 
         RecyclerView recyclerView = (RecyclerView) root.findViewById(R.id.RecyclerView);
         LinearLayoutManager layoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
@@ -104,6 +113,127 @@ public class FindGroup_test extends Fragment {
 
         // Add the request to the RequestQueue.
         requestQueue.add(stringRequest);
+
+
+        post.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+/*
+                RequestQueue requestQueue1;
+                Cache cache1 = new DiskBasedCache(getActivity().getCacheDir(), 1024 * 1024); // 1MB cap
+                Network network1 = new BasicNetwork(new HurlStack());
+                requestQueue1 = new RequestQueue(cache1, network1);
+                requestQueue1.start();
+                String url = "http://52.78.235.23:8080/organization";
+                StringRequest stringRequest1 = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        Log.e("test","post 성공");
+
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }) {
+                    @Override
+                    public Map<String, String> getParams() throws AuthFailureError {
+                        Map<String, String> params = new HashMap<String, String>();
+
+                        params.put("group_name", "dd");
+                        params.put("group_desc", "dd");
+                        params.put("group_category", "eeeee");
+                        params.put("group_image", "dd");
+                        params.put("view_count", "1");
+                        params.put("authority", "1");
+
+                        return params;
+                    }
+                    @Override
+                    public Map<String, String> getHeaders() throws AuthFailureError {
+                        Map<String, String> headers = new HashMap<String, String>();
+                        headers.put("Content-Type", "application/json");
+                        return headers;
+                    }
+                };
+
+
+                // Add the request to the RequestQueue.
+                requestQueue1.add(stringRequest1);
+
+
+            }*/
+                try {
+                    RequestQueue requestQueue2;
+
+                    // Instantiate the cache
+                    Cache cache2 = new DiskBasedCache(getActivity().getCacheDir(), 1024 * 1024); // 1MB cap
+
+                    // Set up the network to use HttpURLConnection as the HTTP client.
+                    Network network2 = new BasicNetwork(new HurlStack());
+
+                    // Instantiate the RequestQueue with the cache and network.
+                    requestQueue2 = new RequestQueue(cache2, network2);
+
+                    // Start the queue
+                    requestQueue2.start();
+
+                    String URL = "http://...";
+                    JSONObject jsonBody = new JSONObject();
+                    jsonBody.put("group_name", "firstvalue");
+                    jsonBody.put("group_desc", "secondobject");
+                    jsonBody.put("group_category", "please");
+                    jsonBody.put("view_count", 77);
+                    jsonBody.put("authority", 1);
+                    final String mRequestBody = jsonBody.toString();
+
+                    StringRequest stringRequest2 = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            Log.e("LOG_VOLLEY", response);
+                        }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Log.e("LOG_VOLLEY", error.toString());
+                        }
+                    }) {
+                        @Override
+                        public String getBodyContentType() {
+                            return "application/json; charset=utf-8";
+                        }
+
+                        @Override
+                        public byte[] getBody() throws AuthFailureError {
+                            try {
+                                return mRequestBody == null ? null : mRequestBody.getBytes("utf-8");
+                            } catch (UnsupportedEncodingException uee) {
+                                VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s", mRequestBody, "utf-8");
+                                return null;
+                            }
+                        }
+
+                        @Override
+                        protected Response<String> parseNetworkResponse(NetworkResponse response) {
+                            String responseString = "";
+                            if (response != null) {
+
+                                responseString = String.valueOf(response.statusCode);
+
+                            }
+                            return Response.success(responseString, HttpHeaderParser.parseCacheHeaders(response));
+                        }
+                    };
+
+                    requestQueue.add(stringRequest);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
 
 
 
