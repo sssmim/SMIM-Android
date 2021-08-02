97d5c9daaf7017dbfaf2a20d46217fb56cf4c0f6
Hyunseung <ardimento22@naver.com>
2021-08-02 16:50:08 +0900

    test 테이블에 대하여 post 성공

--- app/src/main/java/org/techtown/smim/ui/home/test.java
+++ app/src/main/java/org/techtown/smim/ui/home/test.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Cache;
 import com.android.volley.Network;
 import com.android.volley.Request;
@@ -14,18 +15,23 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.BasicNetwork;
 import com.android.volley.toolbox.DiskBasedCache;
 import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 
+import org.json.JSONObject;
 import org.techtown.smim.R;
 import org.techtown.smim.database.group;
 
 import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class test extends AppCompatActivity {
 
@@ -42,48 +48,32 @@ public class test extends AppCompatActivity {
 
         textView = findViewById(R.id.result);
 
-        RequestQueue requestQueue;
+        String url="http://52.78.235.23:8080/test";
+        Map<String,String> map=new HashMap<String,String>();
+        map.put("id", "and test");
+        map.put("pwd", "and test22");
+        JSONObject params = new JSONObject(map);
 
-        // Instantiate the cache
-        Cache cache = new DiskBasedCache(getCacheDir(), 1024 * 1024); // 1MB cap
+        JsonObjectRequest objectRequest=new JsonObjectRequest(Request.Method.POST, url, params,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject obj) {
+                        textView.setText("success");
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                    }
+                }){
 
-        // Set up the network to use HttpURLConnection as the HTTP client.
-        Network network = new BasicNetwork(new HurlStack());
-
-        // Instantiate the RequestQueue with the cache and network.
-        requestQueue = new RequestQueue(cache, network);
-
-        // Start the queue
-        requestQueue.start();
-
-        String url = "http://52.78.235.23:8080/organization";
-
-        // Formulate the request and handle the response.
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
             @Override
-            public void onResponse(String response) {
-                //한글깨짐 - 해결 코드 65~71
-                String changeString = new String();
-                try {
-                    changeString = new String(response.getBytes("8859_1"),"utf-8");
-                } catch (UnsupportedEncodingException e) {
-                    e.printStackTrace();
-                }
-                Gson gson = new GsonBuilder().setPrettyPrinting().create();
-                Type listType = new TypeToken<ArrayList<group>>(){}.getType();
-                list = gson.fromJson(changeString, listType);
-                test = "test2";
-                //textView.setText("" + list.size() + "\n" + test + "\n" + list.get(1).group_desc);
+            public String getBodyContentType() {
+                return "application/json; charset=UTF-8";
             }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-            }
-        });
-
-        textView.setText("" + list.size() + "\n" + test);
-
-        // Add the request to the RequestQueue.
-        requestQueue.add(stringRequest);
+        };
+        objectRequest.setTag("post");
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(objectRequest);
     }
 }
