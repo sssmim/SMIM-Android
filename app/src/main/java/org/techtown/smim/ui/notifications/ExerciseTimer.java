1fcb9282889c79e74014f037f3bcfc60ce4b7f2b
Hyunseung <ardimento22@naver.com>
2021-08-09 16:54:29 +0900

    ieList 에 있는 값 가져옴. iexercise 에 있는 값 가져옴. ieList내 운동이름과 iexercise내 운동이름을 비교하여 같을 경우 해당 시간을 꺼내주는 과정 필요.

--- app/src/main/java/org/techtown/smim/ui/notifications/ExerciseTimer.java
+++ app/src/main/java/org/techtown/smim/ui/notifications/ExerciseTimer.java
@@ -12,17 +12,43 @@ import android.widget.CompoundButton;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
+import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.Cache;
+import com.android.volley.Network;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.StringRequest;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
 import org.techtown.smim.R;
+import org.techtown.smim.database.ielist;
+import org.techtown.smim.database.iexercise;
+import org.techtown.smim.database.video;
+import org.techtown.smim.ui.dashboard.ExercisePlan;
+import org.techtown.smim.ui.dashboard.Youtube;
+import org.techtown.smim.ui.dashboard.YoutubePlan;
 
 public class ExerciseTimer extends AppCompatActivity {
 
+    private TextView exercise_name;
     private TextView countdownText;
+    private TextView secText;
     private ToggleButton start_stop;
 
     private CountDownTimer countDownTimer;
@@ -30,15 +56,133 @@ public class ExerciseTimer extends AppCompatActivity {
     private long time = 0;
     private long tempTime = 0;
 
+    private Integer index = 0;
+
     private boolean firstState = true;
+
+    public List<ielist> list = new ArrayList<>();
+    public List<iexercise> list2 = new ArrayList<>();
+    public List<String> nameList = new ArrayList<>();
+    public List<Integer> countList = new ArrayList<>();
+    public List<String> secList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.timer);
 
-        countdownText = findViewById(R.id.secTextView);
-        start_stop = findViewById(R.id.start_stop);
+        RequestQueue requestQueue;
+        Cache cache = new DiskBasedCache(getCacheDir(), 1024 * 1024); // 1MB cap
+        Network network = new BasicNetwork(new HurlStack());
+        requestQueue = new RequestQueue(cache, network);
+        requestQueue.start();
+
+        String url2 = "http://52.78.235.23:8080/iexercise";
+
+        secText = findViewById(R.id.secTextView);
+
+        StringRequest stringRequest1 = new StringRequest(Request.Method.GET, url2, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                // 한글깨짐 해결 코드
+                String changeString = new String();
+                try {
+                    changeString = new String(response.getBytes("8859_1"), "utf-8");
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+                Gson gson = new GsonBuilder().setPrettyPrinting().create();
+                Type listType = new TypeToken<ArrayList<iexercise>>() {
+                }.getType();
+                list2 = gson.fromJson(changeString, listType);
+
+                for(int i=0; i<list2.size(); i++) {
+                    secList.add(list2.get(i).ie_sec);
+                }
+
+                if(secList.size() != 0) {
+                    secText.setText(secList.get(0));
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+            }
+        });
+
+        String url = "http://52.78.235.23:8080/list";
+
+        exercise_name = findViewById(R.id.exercisename);
+        countdownText = findViewById(R.id.countTextView);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                // 한글깨짐 해결 코드
+                String changeString = new String();
+                try {
+                    changeString = new String(response.getBytes("8859_1"), "utf-8");
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+                Gson gson = new GsonBuilder().setPrettyPrinting().create();
+                Type listType = new TypeToken<ArrayList<ielist>>() {
+                }.getType();
+                list = gson.fromJson(changeString, listType);
+
+                for(int i=0; i<list.size(); i++) {
+                    nameList.add(list.get(i).name1);
+                    nameList.add(list.get(i).name2);
+                    nameList.add(list.get(i).name3);
+                    nameList.add(list.get(i).name4);
+                    nameList.add(list.get(i).name5);
+                }
+
+                if(nameList.size() != 0) {
+                    exercise_name.setText(nameList.get(0));
+                    index++;
+                }
+
+                for(int i=0; i<list.size(); i++) {
+                    countList.add(list.get(i).count1);
+                    countList.add(list.get(i).count2);
+                    countList.add(list.get(i).count3);
+                    countList.add(list.get(i).count4);
+                    countList.add(list.get(i).count5);
+                }
+
+                if(countList.size() != 0) {
+                    countdownText.setText(Integer.toString(countList.get(0)));
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+            }
+        });
+
+        requestQueue.add(stringRequest);
+
+
+
+        requestQueue.add(stringRequest1);
+
+        Button button = findViewById(R.id.gonext);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(nameList.size() != 0) {
+                    exercise_name.setText(nameList.get(index));
+                }
+                if(countList.size() != 0) {
+                    countdownText.setText(Integer.toString(countList.get(index)));
+                }
+                if(secList.size() != 0) {
+                    secText.setText(secList.get(index));
+                }
+                index++;
+            }
+        });
 
         start_stop = (ToggleButton) findViewById(R.id.start_stop);
         start_stop.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -51,9 +195,9 @@ public class ExerciseTimer extends AppCompatActivity {
                     );
 
                     if (firstState) {
-                        String second = countdownText.getText().toString();
+                        String second = secText.getText().toString();
                         time = (Long.parseLong(second) * 1000) + 1000;
-                        countdownText.setText(Long.toString(time));
+                        secText.setText(Long.toString(time));
                     } else {
                         time = tempTime;
                     }
@@ -75,9 +219,6 @@ public class ExerciseTimer extends AppCompatActivity {
                             getResources().
                                     getDrawable(R.drawable.yellow)
                     );
-
-
-                    출처: https://bitsoul.tistory.com/37 [Happy Programmer~]
                     countDownTimer.cancel();
                 }
             }
@@ -85,7 +226,7 @@ public class ExerciseTimer extends AppCompatActivity {
             private void updateTimer() {
                 int seconds = (int) tempTime % 3600000 % 60000 / 1000;
 
-                countdownText.setText(Integer.toString(seconds));
+                secText.setText(Integer.toString(seconds));
             }
         });
     }
