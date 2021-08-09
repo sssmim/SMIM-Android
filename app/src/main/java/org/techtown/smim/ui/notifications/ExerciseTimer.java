2011e3550c712ca5746bf70736ba653f0c2f2765
Hyunseung <ardimento22@naver.com>
2021-08-10 03:54:12 +0900

    Timer - DB 내 ieList에 접근하여 정보 가져와 ie_sec와 연결함. 개인 당 ieList 한 개만 가질 수 있도록 운동 종료 후 해당 리스트 삭제하게 설정해줘야함.

--- app/src/main/java/org/techtown/smim/ui/notifications/ExerciseTimer.java
+++ app/src/main/java/org/techtown/smim/ui/notifications/ExerciseTimer.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import java.io.UnsupportedEncodingException;
@@ -58,13 +59,16 @@ public class ExerciseTimer extends AppCompatActivity {
     private long tempTime = 0;
 
     private Integer index = 0;
+    private Integer Max = 0;
 
     private boolean firstState = true;
 
-    public List<ielist> list = new ArrayList<>();
-    public List<iexercise> list2 = new ArrayList<>();
+    public List<iexercise> list = new ArrayList<>();
+    public List<ielist> list2 = new ArrayList<>();
     public List<String> nameList = new ArrayList<>();
     public List<Integer> countList = new ArrayList<>();
+    public List<String> ie_nameList = new ArrayList<>();
+    public List<String> ie_secList = new ArrayList<>();
     public List<String> secList = new ArrayList<>();
 
     @Override
@@ -72,49 +76,17 @@ public class ExerciseTimer extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.timer);
 
+        secText = findViewById(R.id.secTextView);
+        exercise_name = findViewById(R.id.exercisename);
+        countdownText = findViewById(R.id.countTextView);
+
         RequestQueue requestQueue;
         Cache cache = new DiskBasedCache(getCacheDir(), 1024 * 1024); // 1MB cap
         Network network = new BasicNetwork(new HurlStack());
         requestQueue = new RequestQueue(cache, network);
         requestQueue.start();
 
-        String url2 = "http://52.78.235.23:8080/iexercise";
-
-        secText = findViewById(R.id.secTextView);
-
-        StringRequest stringRequest1 = new StringRequest(Request.Method.GET, url2, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                // 한글깨짐 해결 코드
-                String changeString = new String();
-                try {
-                    changeString = new String(response.getBytes("8859_1"), "utf-8");
-                } catch (UnsupportedEncodingException e) {
-                    e.printStackTrace();
-                }
-                Gson gson = new GsonBuilder().setPrettyPrinting().create();
-                Type listType = new TypeToken<ArrayList<iexercise>>() {
-                }.getType();
-                list2 = gson.fromJson(changeString, listType);
-
-                for (int i = 0; i < list2.size(); i++) {
-                    secList.add(list2.get(i).ie_sec);
-                }
-
-                if (secList.size() != 0) {
-                    secText.setText(secList.get(0));
-                }
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-            }
-        });
-
-        String url = "http://52.78.235.23:8080/list";
-
-        exercise_name = findViewById(R.id.exercisename);
-        countdownText = findViewById(R.id.countTextView);
+        String url = "http://52.78.235.23:8080/iexercise";
 
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
             @Override
@@ -127,33 +99,98 @@ public class ExerciseTimer extends AppCompatActivity {
                     e.printStackTrace();
                 }
                 Gson gson = new GsonBuilder().setPrettyPrinting().create();
-                Type listType = new TypeToken<ArrayList<ielist>>() {
+                Type listType = new TypeToken<ArrayList<iexercise>>() {
                 }.getType();
                 list = gson.fromJson(changeString, listType);
 
                 for (int i = 0; i < list.size(); i++) {
-                    nameList.add(list.get(i).name1);
-                    nameList.add(list.get(i).name2);
-                    nameList.add(list.get(i).name3);
-                    nameList.add(list.get(i).name4);
-                    nameList.add(list.get(i).name5);
+                    ie_nameList.add(list.get(i).ie_name);
+                    ie_secList.add(list.get(i).ie_sec);
                 }
 
-                if (nameList.size() != 0) {
-                    exercise_name.setText(nameList.get(0));
-                }
+                String url2 = "http://52.78.235.23:8080/list";
 
-                for (int i = 0; i < list.size(); i++) {
-                    countList.add(list.get(i).count1);
-                    countList.add(list.get(i).count2);
-                    countList.add(list.get(i).count3);
-                    countList.add(list.get(i).count4);
-                    countList.add(list.get(i).count5);
-                }
+                StringRequest stringRequest2 = new StringRequest(Request.Method.GET, url2, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // 한글깨짐 해결 코드
+                        String changeString = new String();
+                        try {
+                            changeString = new String(response.getBytes("8859_1"), "utf-8");
+                        } catch (UnsupportedEncodingException e) {
+                            e.printStackTrace();
+                        }
+                        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+                        Type listType = new TypeToken<ArrayList<ielist>>() {
+                        }.getType();
+                        list2 = gson.fromJson(changeString, listType);
 
-                if (countList.size() != 0) {
-                    countdownText.setText(Integer.toString(countList.get(0)));
-                }
+                        for (int i = 0; i < list2.size(); i++) {
+                            nameList.add(list2.get(i).name1);
+                            if(list2.get(i).name2 != null) {
+                                nameList.add(list2.get(i).name2);
+                            }
+                            if(list2.get(i).name3 != null) {
+                                nameList.add(list2.get(i).name3);
+                            }
+                            if(list2.get(i).name4 != null) {
+                                nameList.add(list2.get(i).name4);
+                            }
+                            if(list2.get(i).name5 != null) {
+                                nameList.add(list2.get(i).name5);
+                            }
+                        }
+
+                        Max = nameList.size();
+
+                        if (nameList.size() != 0) {
+                            exercise_name.setText(nameList.get(0));
+                        }
+
+                        for (int i = 0; i < list.size(); i++) {
+                            countList.add(list2.get(i).count1);
+                            if(list2.get(i).count2 != null) {
+                                countList.add(list2.get(i).count2);
+                            }
+                            if(list2.get(i).count3 != null) {
+                                countList.add(list2.get(i).count3);
+                            }
+                            if(list2.get(i).count4 != null) {
+                                countList.add(list2.get(i).count4);
+                            }
+                            if(list2.get(i).count5 != null) {
+                                countList.add(list2.get(i).count5);
+                            }
+                        }
+
+                        if (countList.size() != 0) {
+                            countdownText.setText(Integer.toString(countList.get(0)));
+                        }
+
+                        for(int i=0; i<nameList.size(); i++) {
+                            for(int j=0; j<ie_nameList.size(); j++) {
+                                if(nameList.get(i).compareTo(ie_nameList.get(j)) == 0) {
+                                    secList.add(ie_secList.get(j));
+                                }
+                            }
+                        }
+
+                        if (secList.size() != 0) {
+                            secText.setText(secList.get(0));
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                    }
+                });
+
+                requestQueue.add(stringRequest2);
+
+                /*
+                if (secList.size() != 0) {
+                    secText.setText(secList.get(0));
+                }*/
             }
         }, new Response.ErrorListener() {
             @Override
@@ -163,14 +200,26 @@ public class ExerciseTimer extends AppCompatActivity {
 
         requestQueue.add(stringRequest);
 
+        /*
+        RequestQueue requestQueue2;
+        Cache cache2 = new DiskBasedCache(getCacheDir(), 1024 * 1024); // 1MB cap
+        Network network2 = new BasicNetwork(new HurlStack());
+        requestQueue2 = new RequestQueue(cache, network);
+        requestQueue2.start();
+        */
 
-        requestQueue.add(stringRequest1);
 
         Button button = findViewById(R.id.gonext);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 index++;
+                Log.d("test_Max", String.valueOf(Max));
+                Log.d("test_index+1", String.valueOf(index+1));
+                Log.d("test_compare", String.valueOf(Max.compareTo(index+1)));
+                if(Max.compareTo(index+1) == 0) {
+                    Log.d("test_finish", "stop");
+                }
                 if (nameList.size() != 0) {
                     exercise_name.setText(nameList.get(index));
                 }
@@ -224,7 +273,6 @@ public class ExerciseTimer extends AppCompatActivity {
 
             private void updateTimer() {
                 int seconds = (int) tempTime % 3600000 % 60000 / 1000;
-
 
                 if (seconds == 0) {
                     Integer temp = countList.get(index)- 1;
