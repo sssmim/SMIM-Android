80de51560e9f423d1d4c009ae6bf65150842d12f
sia7997 <sia7997@naver.com>
2021-09-29 23:55:18 +0900

    1.등업페이지 xml완료 2.등업페이지 내정보가져오기(포인트,grage제외) 3.등급별이미지디자인 완료

--- app/src/main/java/org/techtown/smim/ui/MyPage/UpgradeFragment.java
+++ app/src/main/java/org/techtown/smim/ui/MyPage/UpgradeFragment.java
@@ -7,13 +7,42 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.android.volley.Cache;
+import com.android.volley.Network;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.StringRequest;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 
 import org.techtown.smim.R;
+import org.techtown.smim.database.board;
+import org.techtown.smim.database.comment;
+import org.techtown.smim.database.personal;
+
+import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class UpgradeFragment extends Fragment {
 
-
+    public List<personal> list = new ArrayList<>();
+    public List<personal> list1 = new ArrayList<>();
+    List<board> list2 = new ArrayList<>();
+    List<comment> list3 = new ArrayList<>();
+    String  Id;
+    Integer boardcount=0;
+    Integer commentcount=0;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -22,7 +51,135 @@ public class UpgradeFragment extends Fragment {
         View root =inflater.inflate(R.layout.fragment_upgrade, container, false);
 
 
+        Bundle bundle = getArguments();
+        Long mem_num = bundle.getLong("mem_num");
 
+
+        TextView name = root.findViewById(R.id.upgradename);
+        TextView boardc = root.findViewById(R.id.nowboard);
+        TextView commentc = root.findViewById(R.id.nowcomment);
+
+        RequestQueue requestQueue1;
+        Cache cache1 = new DiskBasedCache(getActivity().getCacheDir(), 1024 * 1024); // 1MB cap
+        Network network1 = new BasicNetwork(new HurlStack());
+        requestQueue1 = new RequestQueue(cache1, network1);
+        requestQueue1.start();
+
+        String url1 = "http://52.78.235.23:8080/personal";
+
+        StringRequest stringRequest1 = new StringRequest(Request.Method.GET, url1, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                // 한글깨짐 해결 코드
+                String changeString = new String();
+                try {
+                    changeString = new String(response.getBytes("8859_1"),"utf-8");
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+                Gson gson = new GsonBuilder().setPrettyPrinting().create();
+                Type listType = new TypeToken<ArrayList<personal>>(){}.getType();
+                list1 = gson.fromJson(changeString, listType);
+                for(int i=0; i<list1.size(); i++) {
+
+                    if (list1.get(i).mem_num.compareTo(mem_num) == 0) {
+
+                        name.setText(list1.get(i).name);
+
+
+                    }
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+            }
+        });
+
+        requestQueue1.add(stringRequest1);
+
+
+        RequestQueue requestQueue2;
+        Cache cache2 = new DiskBasedCache(getActivity().getCacheDir(), 1024 * 1024); // 1MB cap
+        Network network2 = new BasicNetwork(new HurlStack());
+        requestQueue2 = new RequestQueue(cache2, network2);
+        requestQueue2.start();
+
+        String url2 = "http://52.78.235.23:8080/board";
+
+        StringRequest stringRequest2 = new StringRequest(Request.Method.GET, url2, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                // 한글깨짐 해결 코드
+                String changeString = new String();
+                try {
+                    changeString = new String(response.getBytes("8859_1"),"utf-8");
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+                Gson gson = new GsonBuilder().setPrettyPrinting().create();
+                Type listType = new TypeToken<ArrayList<board>>(){}.getType();
+                list2 = gson.fromJson(changeString, listType);
+                for(int i=0; i<list2.size(); i++) {
+
+                    if (list2.get(i).p_num.compareTo(mem_num) == 0) {
+
+                        boardcount+=1;
+
+
+                    }
+                }
+                boardc.setText(boardcount.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+            }
+        });
+
+        requestQueue2.add(stringRequest2);
+
+
+        RequestQueue requestQueue3;
+        Cache cache3 = new DiskBasedCache(getActivity().getCacheDir(), 1024 * 1024); // 1MB cap
+        Network network3 = new BasicNetwork(new HurlStack());
+        requestQueue3 = new RequestQueue(cache3, network3);
+        requestQueue3.start();
+
+        String url3 = "http://52.78.235.23:8080/comment";
+
+        StringRequest stringRequest3 = new StringRequest(Request.Method.GET, url3, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                // 한글깨짐 해결 코드
+                String changeString = new String();
+                try {
+                    changeString = new String(response.getBytes("8859_1"),"utf-8");
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+                Gson gson = new GsonBuilder().setPrettyPrinting().create();
+                Type listType = new TypeToken<ArrayList<comment>>(){}.getType();
+                list3 = gson.fromJson(changeString, listType);
+                for(int i=0; i<list3.size(); i++) {
+
+                    if (list3.get(i).p_num.compareTo(mem_num) == 0) {
+
+                        commentcount+=1;
+
+
+                    }
+                }
+                commentc.setText(commentcount.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+            }
+        });
+
+        requestQueue3.add(stringRequest3);
 
 
 
