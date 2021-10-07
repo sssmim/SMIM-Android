1c2474e71ba3d3e739118028a2c31e02370b5d46
Hyunseung <ardimento22@naver.com>
2021-10-07 18:34:51 +0900

    가로 회전 기능 추가

--- app/src/main/java/org/techtown/smim/ui/dashboard/GroupExercisePlay.java
+++ app/src/main/java/org/techtown/smim/ui/dashboard/GroupExercisePlay.java
@@ -69,12 +69,10 @@ public class GroupExercisePlay extends AppCompatActivity implements AutoPermissi
         setContentView(R.layout.activity_group_exercise_play);
         YouTubePlayerView youtubePV = findViewById(R.id.youtube_player_view);
 
-
-
         Intent intent = getIntent();
 
         String url = intent.getExtras().getString("url");
-Long mem_num = intent.getExtras().getLong("mem");
+        Long mem_num = intent.getExtras().getLong("mem");
         youtubePV.play(url,null);
 
         FrameLayout previewFrame = findViewById(R.id.previewFrame);
@@ -91,7 +89,6 @@ Long mem_num = intent.getExtras().getLong("mem");
 
                 Long now = System.currentTimeMillis();
                 date1 = new Date(now);
-
             }
         });
 
